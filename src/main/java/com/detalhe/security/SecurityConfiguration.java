5d3fa1e8e77437a8494ca879a1d8571ea3f17649
Marco <marcoar1978@gmail.com>
2020-01-23 11:28:30 -0300

    Ajuste do CORS

--- src/main/java/com/detalhe/security/SecurityConfiguration.java
+++ src/main/java/com/detalhe/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.detalhe.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.detalhe.repository.UsuarioRepository;
 
@@ -45,14 +50,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// Configurações de Autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        .and().csrf().disable();
+
+		
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/hello").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.anyRequest().authenticated()
-		.and().csrf().disable()
+		.anyRequest().authenticated().and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+   }
 
 	// Configuração de recursos estáticos
 	@Override
